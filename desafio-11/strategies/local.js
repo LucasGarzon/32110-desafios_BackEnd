3b715c97e06aee747b280698d51e5f150e44feb9
Lucas Garzon <garzon.lucas.m@gmail.com>
2022-11-22 21:52:36 -0300

    feat: desafio-11 => descomentado

--- desafio-11/strategies/local.js
+++ desafio-11/strategies/local.js
@@ -16,20 +16,6 @@ passport.use(new LocalStrategy(function (username, password, done) {
   })
 }))
 
-// passport.use(new LocalStrategy(function (username, password, done) {
-// 	userModel.findOne({ username: username }, function (err, user) {
-// 		if (err) return done(err);
-// 		if (!user) return done(null, false, { message: 'Incorrect username.' });
-
-// 		bcrypt.compare(password, user.password, function (err, res) {
-// 			if (err) return done(err);
-// 			if (res === false) return done(null, false, { message: 'Incorrect password.' });
-			
-// 			return done(null, user);
-// 		});
-// 	});
-// }));
-
 passport.serializeUser((user, done) => done(null, user.id))
 
 passport.deserializeUser((id, done) => {
