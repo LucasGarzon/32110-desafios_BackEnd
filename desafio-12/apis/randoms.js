505f4dd3f923387d28fbed0f24e8b2499bb29702
Lucas Garzon <garzon.lucas.m@gmail.com>
2022-11-27 19:26:18 -0300

    feat: desafio-12 => Fork y child process implementado

--- desafio-12/apis/randoms.js
+++ desafio-12/apis/randoms.js
@@ -1,10 +1,15 @@
-export default function getRandomInt(amount) {
+let data = parseInt(process.argv.slice(2))
+
+function getRandomInt(amount) {
   const enteredAmount = amount || 100000000
   for (let index = 0; index < enteredAmount ; index++) {
     Math.floor(Math.random() * 1000 + 1)
   }
-  return {message: 'task completed'}
+  const result = {message: 'task completed', amount: enteredAmount}
+  return process.send(result)
 }
+
+getRandomInt(data)
 
 
 
--- desafio-12/routes/randoms-router.js
+++ desafio-12/routes/randoms-router.js
@@ -1,13 +1,17 @@
-import getRandomInt from "../apis/randoms.js";
+// import getRandomInt from "../apis/randoms.js";
 import { Router } from "express";
+import { fork } from 'child_process'
 
 const router = new Router 
 
 router.get('/', (req, res) => {
-  const userAmount = req.query.cant || 'A default value was used'
-  const taskCompleted = getRandomInt(userAmount)
-  res.render('index_random', {message: taskCompleted.message, amount: userAmount})
+  const userAmount = parseInt(req.query.cant)
+  let taskCompleted = fork('./apis/randoms', [userAmount])
+  taskCompleted.on('message', msg => {
+    return res.render('index_random', {message: msg.message, amount: msg.amount})
+  })
 })
 
 const randomRouter = router
 export {randomRouter}
+
