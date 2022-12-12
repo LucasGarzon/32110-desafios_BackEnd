d04b94638d5fb5ece55e0b44c047edd925a8a641
Lucas Garzon <garzon.lucas.m@gmail.com>
2022-12-12 15:40:16 -0300

    feat: desafio-13 => node cluster implementado

--- desafio-13/app.js
+++ desafio-13/app.js
@@ -15,27 +15,21 @@ import * as dotenv from 'dotenv'
 import userModel from './models/User.js'
 import './strategies/local.js'
 import { randomRouter } from './routes/randoms-router.js'
+import cluster from 'cluster'
+import core from 'os'
 dotenv.config()
 
 const uri = process.env.USER_URI
-
 loader.start()
-
 const productManager = new ProductManager()
 const chatManager = new ChatManager()
-
 const app = express()
 const PORT = parseInt(process.argv.slice(2)) || 8080
-const server = app.listen(PORT, () => console.log(`Server up on port ${PORT}`))
-const io = new Server(server)
-
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/content', express.static('./public'))
 app.set('views', './views')
 app.set('view engine', 'ejs')
-
-//IMPLEMENTACION DEL DESAFIO 11 --------------------------
 app.use(cookieParser())
 app.use(session({
   store: MongoStore.create({
@@ -52,89 +46,88 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-const logChecker = (req, res, next) => {
-  if (req.isAuthenticated()) return next()
-  res.redirect('/login')
-}
+// ####--------- DESAFIO 13 ----------####
 
-const outlineChecker = (req, res, next) => {
-  if (!req.isAuthenticated()) return next()
-  res.redirect('/')
-}
-
-app.get('/', logChecker, (req, res) => {
-  res.render('index', {user: req.user.username})
-})
-
-app.get('/dashboard', logChecker, (req, res) => {
-    res.render('index', {user: req.user.username})
-})
-
-app.get('/singup', (req, res) => {
-  res.render('index_singup')
-})
-
-app.get('/registerError', (req, res) => {
-  res.render('register_error')
-})
-
-app.get('/loginError', (req, res) => {
-  res.render('login_error')
-})
-
-app.post('/singup', async (req, res) => {
-  const {username, email, password} = req.body
-  try {
-    let user = await userModel.findOne({username})
-    if(user) res.redirect('/registerError')
-    const cryptPass = await bcrypt.hash(password, 12)
-    user = await userModel.create({
-      username,
-      email,
-      password: cryptPass
-    })
-    res.redirect('/')
-  } catch (err) {
-    console.log(err);
+if (cluster.isPrimary) {
+  console.log(`Primary process ${process.pid}`)
+  for (let i=0; i<core.cpus().length; i++) {
+    cluster.fork()
   }
-})
-
-app.get('/login', outlineChecker, (req, res) => {  
-  res.render('index_login');
-})
-
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/dashboard',
-  failureRedirect: '/loginError'
-}))
-
-app.get('/logout', function (req, res, next) {
-  const username = req.user.username
-	req.logout(function(err) {
-    if (err) { 
-      return next(err); 
+  cluster.on('exit', () => cluster.fork())
+} else {
+  const server = app.listen(PORT, () => console.log(`Server up on port ${PORT} by process ${process.pid}`))
+  const io = new Server(server)
+  const logChecker = (req, res, next) => {
+    if (req.isAuthenticated()) return next()
+    res.redirect('/login')
+  }
+  const outlineChecker = (req, res, next) => {
+    if (!req.isAuthenticated()) return next()
+    res.redirect('/')
+  }
+  app.get('/', logChecker, (req, res) => {
+    res.render('index', {user: req.user.username})
+  })
+  app.get('/dashboard', logChecker, (req, res) => {
+      res.render('index', {user: req.user.username})
+  })
+  app.get('/singup', (req, res) => {
+    res.render('index_singup')
+  })
+  app.get('/registerError', (req, res) => {
+    res.render('register_error')
+  })
+  app.get('/loginError', (req, res) => {
+    res.render('login_error')
+  })
+  app.post('/singup', async (req, res) => {
+    const {username, email, password} = req.body
+    try {
+      let user = await userModel.findOne({username})
+      if(user) res.redirect('/registerError')
+      const cryptPass = await bcrypt.hash(password, 12)
+      user = await userModel.create({
+        username,
+        email,
+        password: cryptPass
+      })
+      res.redirect('/')
+    } catch (err) {
+      console.log(err);
     }
-    res.render('index_logout', {user: username})
   })
-})
-
-//-----------------------------------------------------------
-
-app.use('/info', infoRouter)
-app.use('/api/random', randomRouter)
-app.use('/products', productRouter)
-app.use('/chat', chatRouter)
-
-io.on('connection', socket => {
-  console.log(`Client ${socket.id} connected...`)
-  productManager.findAll().then(result => socket.emit('history', result))
-                          .catch(err => console.log(err))
-  chatManager.findAll().then(result => socket.emit('chatHistory', result))
-                       .catch(err => console.log(err))
-  socket.on('products', data => {
-      io.emit('history', data)
+  app.get('/login', outlineChecker, (req, res) => {  
+    res.render('index_login');
   })
-  socket.on('chat', data => {
-      io.emit('chatHistory', data)
+  app.post('/login', passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/loginError'
+  }))
+  app.get('/logout', function (req, res, next) {
+    const username = req.user.username
+    req.logout(function(err) {
+      if (err) { 
+        return next(err); 
+      }
+      res.render('index_logout', {user: username})
+    })
   })
-})
+  app.use('/info', infoRouter)
+  app.use('/api/random', randomRouter)
+  app.use('/products', productRouter)
+  app.use('/chat', chatRouter)
+  io.on('connection', socket => {
+    console.log(`Client ${socket.id} connected...`)
+    productManager.findAll().then(result => socket.emit('history', result))
+                            .catch(err => console.log(err))
+    chatManager.findAll().then(result => socket.emit('chatHistory', result))
+                         .catch(err => console.log(err))
+    socket.on('products', data => {
+        io.emit('history', data)
+    })
+    socket.on('chat', data => {
+        io.emit('chatHistory', data)
+    })
+  })
+}
+
--- desafio-13/daos/loaderMongo.js
+++ desafio-13/daos/loaderMongo.js
@@ -7,6 +7,5 @@ const uri = process.env.USER_URI
 export default async function startDB(){
   await mongoose.connect(uri, (err) => {
     if (err) return console.log('error al conectar a la base de datos')
-    console.log('base de datos Mongo conectada!');
   })
 }
